1d1d422c8211e3d64161ebcb028153c9412bb058
Your Name <you@example.com>
2026-02-02 03:55:12 +0000

    updated the code

--- hello_world.py
+++ hello_world.py
@@ -2,3 +2,11 @@
 # This is a simple Python script to print "Hello, World!"
 
 print("Hello, World!")
+
+
+# Ask the user for their name
+name = input("What's your name? ")
+
+# Print a personalized message
+print(f"Nice to meet you, {name}!")
+
